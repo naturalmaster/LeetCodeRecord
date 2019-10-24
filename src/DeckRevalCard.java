ea31de714f47194b4c9a40cfa3c299c323d8e5de
huangFuJin <huangfujin@onepaypass.com>
2019-10-24 20:34:57 +0800

    力扣950
    1. 卡牌魔术
    2. 思路是利用一个队列将这次不用的存起来，然后下次再递归的选出
    3. 未完成

---
+++ src/DeckRevalCard.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class DeckRevalCard {
+
+
+    public int[] deckRevealedIncreasing(int[] deck) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Arrays.sort(deck);
+        int i=0;
+        Queue<Integer> queue = new LinkedList<>();
+
+    }
+
+    public int[] orderIndex(int[] array){
+        int[] result = new int[array.length];
+        int remainIndex = 1;
+        int i = 0;
+        while(i < array.length) {
+
+        }
+    }
+}
